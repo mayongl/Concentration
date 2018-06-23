56757621418f4fc8eb250554255ceca6671fb360
mayonglin <mayonglin@lantogis.com>
2018-06-23 10:41:00 +0800

    Add cards.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var cardButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+    }
+    
 }
 
